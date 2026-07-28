f1c560a3348efb89df3a7a41402297b86c856288
feehisamuel <stfeehi001@st.ug.edu.gh>
2026-07-28 22:33:44 +0000

    Update assignment_03_array_statistics.py

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -39,3 +39,45 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def calculate_sum(numbers):
+        total = 0
+        for number in numbers:
+                      total += number
+        return total
+
+def calculate_average(numbers):
+        total = calculate_sum(numbers)
+        return total / len(numbers)
+
+def find_maximum(numbers):
+    maximum = numbers[0]
+
+    for number in numbers:
+        if number > maximum:
+            maximum = number
+
+    return maximum
+
+
+def find_minimum(numbers):
+    minimum = numbers[0]
+
+    for number in numbers:
+        if number < minimum:
+            minimum = number
+
+    return minimum
+
+n = int(input("How many numbers do you want to enter? "))
+if n <= 0:
+    print("Error: N must be a positive integer.")
+else:
+
+    numbers = []
+    for i in range(n):
+         number = float(input(f"Enter number {i + 1}: "))
+         numbers.append(number)
+    print("Sum:", calculate_sum(numbers))
+    print("Average:", calculate_average(numbers))
+    print("Maximum:", find_maximum(numbers))
+    print("Minimum:", find_minimum(numbers))
